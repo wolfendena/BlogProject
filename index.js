0c7b7760d40836e41dcee1a6bcd58a2bf02cb02f
Alex <alexwolfenden@outlook.com>
2019-07-10 20:57:57 +0400

    first file of tuesday

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const urlEncoded = bodyParser.urlencoded({extended: false})
-
+const mysql = require('mysql')
 const dummyData = [{blogTitle: "Work on my blog", blogBody: "Working" }];
+
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'admin',
+  password : '145520Aw',
+  database : 'BlogProject'
+});
+
+db.connect(function(err){
+  if (err) throw (err);
+  console.log("DB is connected...")
+});
+
 
 //setting up
 const app = express();
@@ -17,18 +30,34 @@ app.use(express.static('./public'));
 
 //Get for Tasks: returns all tasks
 app.get('/myblog', (req, res) => {
+  let sql = 'SELECT * FROM blog';
+  db.query(sql, function (err, results) {
+    if (err) throw err;
     //rendering tasks view and passing taskToDo data
-    res.render('myblog', {taskToDo: dummyData});
+    res.render('myblog', {taskToDo: results});
+  });
 });
 
 // Post for tasks: posting a task
-app.post('/tasks', urlEncoded, function(req, res){
-  //formatting for incoming data to add to my dataset
-  let incomingItem = {}
-  incomingItem.taskItem = req.body.task
-  dummyData.push(incomingItem)
-  res.redirect('/myblog')
-});
+// app.post('/tasks', urlEncoded, function(req, res){
+//   //formatting for incoming data to add to my dataset
+//   let incomingItem = {}
+//   incomingItem.taskItem = req.body.task
+//   db.push(incomingItem)
+// res.redirect('/myblog')
+// });
+
+app.post('/tasks', urlEncoded, (req, res) => {
+  let task = req.body
+  let sql = 'INSERT INTO task SET ?' ;
+  db.query(sql, task, function (err, results) {
+      if (err) throw err;
+      // rendering tasks view and passing taskToDo data
+      console.log(results)
+      res.redirect('/myblog')
+    });
+  });
+
 
 //Delete for tasks: deleting specific tasks
 app.delete("/tasks/:id", function(req, res){
--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ const taskItems = document.getElementsByTagName('li');
 for (let i = 0; i < taskItems.length; i++) {
     const element = taskItems[i];
     element.addEventListener('click', function(){
-        
+        console.log(element.id)
         fetch('/tasks/' + element.id, { method: "delete" } )
         .then((res) => res.json() )
         .then((data) => location.reload() )
